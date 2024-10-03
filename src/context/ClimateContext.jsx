b265fcd28be7de5319f57a6cb5f013148ac4c1b9
MynnTapp <Desmynnmjc@gmail.com>
2024-10-03 16:10:14 -0500

    Andrew's turn again

--- src/context/ClimateContext.jsx
+++ src/context/ClimateContext.jsx
@@ -1,2 +1,17 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+import { useContext, useState, createContext } from "react";
+
+const ClimateContext = createContext();
+const userClimateContext = () => {
+  return useContext(ClimateContext);
+};
+
+export default function ClimateProvider(props) {
+  const [temperature, setTemperature] = useState(50);
+  const [humidity, setHumidity] = useState(40);
+  console.log(humidity);
+  console.log(temperature);
+
+  return <ClimateContext.Provider>{props.children}</ClimateContext.Provider>;
+}
